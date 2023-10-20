1530b77ee6b2bf96d8e19b9a845163858d488623
Bernhard Schuster <bernhard@ahoi.io>
2023-10-20 18:50:37 +0200

    upsi

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ pub mod runtime {
 	include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 }
 
+#[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _ = OnlineClient::<PolkadotConfig>::new().await?;
     Ok(())
